341ee726cdcc09db815002b51996d9c91b228849
DmytroBerezovskyi <berezovscky.dmitrij1@gmail.com>
2021-09-05 08:35:58 +0300

    Final version

---
+++ Sequences.py
@@ -0,0 +1,30 @@
+task = 0
+amount = 0
+suma = 0
+rav_max = 0
+rav_min = 5000000000000000000000000000
+even = 0
+odd = 0
+while True:
+    enter = int(input('Введите пожалуйста целое число: '))
+    suma = suma + enter
+    if enter == task:
+        break
+    else:
+        amount = amount + 1     # Подсчет введенных чисел
+    if enter > rav_max:         # Поиск максимального значения
+        rav_max = enter
+    if enter < rav_min:         # Поиск минимального значения
+        rav_min = enter
+    if enter % 2 == 0:          # Подсчет четных и нечетных чисел
+         even = even + 1
+    else:
+        odd = odd + 1
+
+average = suma / (amount+1)      # Подсчет среднего арифметическое всех введённых чисел
+
+print('Kоличество введённых чисел: ',amount)
+print('Сумма введённых чисел: ',suma)
+print('Cреднее арифметическое всех введённых чисел: ',average)
+print('Максимальное и минимальное значение: ', rav_max, '\t', rav_min)
+print('Kоличество чётных и не чётных значений: ', even, '\t', odd)
