aa2756de8f0730c8fb23500f162f6c09de9ca239
DmytroBerezovskyi <berezovscky.dmitrij1@gmail.com>
2021-09-08 10:38:40 +0300

    Minimum and maximum limits have been changed

--- Sequences.py
+++ Sequences.py
@@ -2,7 +2,7 @@ task = 0
 amount = 0
 suma = 0
 rav_max = 0
-rav_min = 5000000000000000000000000000
+rav_min = 0
 even = 0
 odd = 0
 while True:
@@ -12,8 +12,12 @@ while True:
         break
     else:
         amount = amount + 1     # Подсчет введенных чисел
+    if rav_max == 0:
+        rav_max = enter
     if enter > rav_max:         # Поиск максимального значения
         rav_max = enter
+    if rav_min == 0:
+        rav_min = enter
     if enter < rav_min:         # Поиск минимального значения
         rav_min = enter
     if enter % 2 == 0:          # Подсчет четных и нечетных чисел
@@ -23,8 +27,8 @@ while True:
 
 average = suma / (amount+1)      # Подсчет среднего арифметическое всех введённых чисел
 
-print('Kоличество введённых чисел: ',amount)
-print('Сумма введённых чисел: ',suma)
-print('Cреднее арифметическое всех введённых чисел: ',average)
+print('Kоличество введённых чисел: ', amount)
+print('Сумма введённых чисел: ', suma)
+print('Cреднее арифметическое всех введённых чисел: ', average)
 print('Максимальное и минимальное значение: ', rav_max, '\t', rav_min)
 print('Kоличество чётных и не чётных значений: ', even, '\t', odd)
